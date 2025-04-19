f748ce32ab6d97db687b334e3f1731000fadedce
irodamirzaxmatova880 <irodamirzaxmatova880@gmail.com>
2025-04-19 09:13:58 +0000

    feat: Add migrations for users, password reset tokens, and sessions tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->timestamps();
         });
 
+
+        
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
