483e47f9edbab988d2b2a8a832e762e14a8432eb
irodamirzaxmatova880 <irodamirzaxmatova880@gmail.com>
2025-04-19 09:21:27 +0000

    Update routes with authentication and guest middleware groups

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
+
+
+Route::resource('/users',UserController::class)->names('users');
+Route::resource('/posts',PostController::class)->names('posts');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get( '/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::get( '/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+});
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [UserController::class, 'index'])->name('users.index');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    Route::get('users/create', [UserController::class,'create'])->name('users.create');
+    Route::post('users/create', [UserController::class,'store'])->name('users.store');
+
+});
+
+
