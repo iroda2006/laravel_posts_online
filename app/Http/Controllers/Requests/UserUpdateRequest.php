1163ba333b95bd776185cba63401693760245da5
irodamirzaxmatova880 <irodamirzaxmatova880@gmail.com>
2025-04-19 09:09:18 +0000

    feat: Add relationships and fillable attributes to User model

--- app/Http/Controllers/Requests/UserUpdateRequest.php
+++ app/Http/Controllers/Requests/UserUpdateRequest.php
@@ -1,25 +1,58 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Models;
 
-use Illuminate\Foundation\Http\FormRequest;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class UserUpdateRequest extends FormRequest
+class User extends Authenticatable
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
-    public function rules(): array
-    {
-        $userId = $this->user()->id;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        // 'image',    
+    ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * 
+     * 
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
         return [
-            'username' => 'required|string|max:255|unique:users,username,' . $userId,
-            'email' => 'required|email|unique:users,email,' . $userId,
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
         ];
     }
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function image(){
+        return $this->morphOne(Image::class,'imageable');
+    }
 }
-
